e123b6d2f1c7fd31f8788419567606844a8ec952
llizzza <bondarenkolisa18a@gmail.com>
2025-11-17 09:50:44 +0500

    Update DeletingValue.java

--- DeletingValue.java
+++ DeletingValue.java
@@ -1,35 +1,9 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class DeletingValue {
 
-    private List<Integer> list;
-    private int valueToRemove;
-
-    public DeletingValue(List<Integer> list, int valueToRemove) {
-        this.list = new ArrayList<>(list);
-        this.valueToRemove = valueToRemove;
+    public static <T> void removeAllOccurrences(List<T> list, T value) {
+        list.removeIf(element -> element.equals(value));
     }
 
-    public void removeAll() {
-        list.removeIf(x -> x == valueToRemove);
-    }
-
-    @Override
-    public String toString() {
-        return "Список после удаления: " + list;
-    }
-
-    public static int readInt(Scanner sc, String msg) {
-        while (true) {
-            System.out.print(msg);
-            if (sc.hasNextInt()) {
-                return sc.nextInt();
-            } else {
-                System.out.println("Введите целое число!");
-                sc.next();
-            }
-        }
-    }
 }
